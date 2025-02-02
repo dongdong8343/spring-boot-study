eae3d73aa7dc7c504efeda832be15de776594d1b
dongdong8343 <fkdlem8343@naver.com>
2025-02-02 17:17:13 +0900

    PostsRepository 테스트 코드 작성

--- src/test/java/org/example/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/org/example/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,6 +1,5 @@
 package org.example.springboot.domain.posts;
 
-import org.aspectj.lang.annotation.After;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
