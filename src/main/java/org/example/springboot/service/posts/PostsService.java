9c12738a214cde0509340c1c798c69902dd56ef6
dongdong8343 <fkdlem8343@naver.com>
2025-02-03 22:28:35 +0900

    PostsService 게시글 조회 기능 생성

---
+++ src/main/java/org/example/springboot/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package org.example.springboot.service.posts;
+
+import lombok.RequiredArgsConstructor;
+import org.example.springboot.domain.posts.Posts;
+import org.example.springboot.domain.posts.PostsRepository;
+import org.example.springboot.web.dto.PostsResponseDto;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
