aff91af74d499191ba6498e39e888817e10be7ad
dongdong8343 <fkdlem8343@naver.com>
2025-02-08 17:30:36 +0900

    게시글 등록 test 제거

--- src/test/java/org/example/springboot/web/PostApiControllerTest.java
+++ src/test/java/org/example/springboot/web/PostApiControllerTest.java
@@ -49,36 +49,4 @@ public class PostApiControllerTest {
         // when
         ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
     }
-  
-    @AfterEach
-    public void tearDown() throws Exception {
-        postsRepository.deleteAll();
-    }
-
-    @Test
-    public void Posts_등록된다() throws Exception {
-        // given
-        String title = "title";
-        String content = "content";
-        PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
-                .title(title)
-                .content(content)
-                .author("author")
-                .build();
-
-        String url = "http://localhost:" + port + "/api/v1/posts";
-
-        // when
-        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
-
-        // then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(responseEntity.getBody()).isGreaterThan(0L);
-
-        List<Post> all = postsRepository.findAll();
-        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
-        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
-        assertThat(all.get(0).getTitle()).isEqualTo(title);
-        assertThat(all.get(0).getContent()).isEqualTo(content);
-    }
 }
