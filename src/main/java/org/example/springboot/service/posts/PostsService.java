8a23a3656811a983aade237d904a0f2e5f6cc199
dongdong8343 <fkdlem8343@naver.com>
2025-02-11 23:46:58 +0900

    게시글 조회 화면

--- src/main/java/org/example/springboot/service/posts/PostsService.java
+++ src/main/java/org/example/springboot/service/posts/PostsService.java
@@ -4,7 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.example.springboot.domain.posts.Post;
 import org.example.springboot.domain.posts.PostsRepository;
 import org.example.springboot.web.dto.PostResponseDto;
+import org.example.springboot.web.dto.PostsListResponseDto;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -16,5 +21,12 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return PostResponseDto.fromEntity(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
