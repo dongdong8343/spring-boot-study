57c44d983a4142f79858a740b01e7d18378820e6
dongdong8343 <fkdlem8343@naver.com>
2025-02-12 21:13:57 +0900

    게시글 삭제 화면 및 api 개발

--- src/main/java/org/example/springboot/service/posts/PostsService.java
+++ src/main/java/org/example/springboot/service/posts/PostsService.java
@@ -3,6 +3,7 @@ package org.example.springboot.service.posts;
 import lombok.RequiredArgsConstructor;
 import org.example.springboot.domain.posts.Post;
 import org.example.springboot.domain.posts.PostsRepository;
+import org.example.springboot.web.dto.PostsResponseDto;
 import org.example.springboot.web.dto.PostsUpdateRequestDto;
 import org.example.springboot.web.dto.PostsSaveRequestDto;
 import org.springframework.stereotype.Service;
@@ -21,7 +22,21 @@ public class PostsService {
         return id;
     }
 
+    @Transactional(readOnly = true)
+    public PostsResponseDto findById(Long id){
+        Post entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Post post = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+        postsRepository.delete(post);
     }
 }
--- src/main/java/org/example/springboot/web/PostsApiController.java
+++ src/main/java/org/example/springboot/web/PostsApiController.java
@@ -3,12 +3,8 @@ package org.example.springboot.web;
 import lombok.RequiredArgsConstructor;
 import org.example.springboot.service.posts.PostsService;
 import org.example.springboot.web.dto.PostsUpdateRequestDto;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
 import org.example.springboot.web.dto.PostsSaveRequestDto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
@@ -23,5 +19,11 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    @DeleteMapping("api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
     }
 }
