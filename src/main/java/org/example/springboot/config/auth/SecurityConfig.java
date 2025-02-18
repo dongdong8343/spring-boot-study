e6fe8d69f8b78fa7bcd9e2c43d6e34031b5000cc
dongdong8343 <fkdlem8343@naver.com>
2025-02-18 09:52:56 +0900

    SecurityConfig 설정

--- src/main/java/org/example/springboot/config/auth/SecurityConfig.java
+++ src/main/java/org/example/springboot/config/auth/SecurityConfig.java
@@ -2,6 +2,7 @@ package org.example.springboot.config.auth;
 
 import lombok.RequiredArgsConstructor;
 import org.example.springboot.domain.user.Role;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
