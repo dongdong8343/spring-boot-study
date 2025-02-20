184a3db15f3338641afdc274fa9dddcd6da9fb1a
dongdong8343 <fkdlem8343@naver.com>
2025-02-20 21:05:08 +0900

    네이버 로그인 구현

--- src/main/java/org/example/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/org/example/springboot/config/auth/dto/OAuthAttributes.java
@@ -23,11 +23,21 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
+        if("naver".equals(registrationId)){
+            return ofNaver("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes){
         return new OAuthAttributes(attributes, userNameAttributeName, (String) attributes.get("name"), (String) attributes.get("email"), (String) attributes.get("picture"));
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes){
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return new OAuthAttributes(response, userNameAttributeName, (String) response.get("name"), (String) response.get("email"), (String) response.get("picture"));
     }
 
     public User toEntity(){
