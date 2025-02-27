31843ef102176524b9c797def8e66cb84a22e43a
dongdong8343 <fkdlem8343@naver.com>
2025-02-27 20:27:26 +0900

    soft delete 적용

--- src/main/java/org/example/springboot/domain/posts/Post.java
+++ src/main/java/org/example/springboot/domain/posts/Post.java
@@ -6,10 +6,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.springboot.domain.BaseTimeEntity;
 import org.example.springboot.web.dto.UpdatePost;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 import java.util.Arrays;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE post set is_deleted = true WHERE id = ?")
+@SQLRestriction("is_deleted = false")
 @Entity
 public class Post extends BaseTimeEntity {
     @Id
@@ -24,15 +28,18 @@ public class Post extends BaseTimeEntity {
 
     private String author;
 
-    private Post(String title, String content, String author) {
+    private Boolean is_deleted;
+
+    private Post(String title, String content, String author, Boolean is_deleted) {
         this.title = title;
         this.content = content;
         this.author = author;
+        this.is_deleted = is_deleted;
     }
 
     // 정적 팩토리 메서드
     public static Post ofPosts(String title, String content, String author) {
-        return new Post(title, content, author);
+        return new Post(title, content, author, false);
     }
 
     public void update(UpdatePost.Request request) {
